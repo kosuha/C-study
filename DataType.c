77be5e206318f160169932b2e029d900c4036f4c
Seonho Kim <okeydokekim@gmail.com>
2021-04-09 18:26:10 +0900

    data type - float, double

--- DataType.c
+++ DataType.c
@@ -1,5 +1,7 @@
 /* 
-변수는 메모리, 메로리는 주소를 가진다.
+변수는 메모리, 메모리는 주소를 가진다.
+float은 소수점이하 6자리까지 double은 15자리까지 근삿값에 의한 오류가 없다. (double만 사용)
+문자열은 문자의 배열이다.
 */
 
 #include <stdio.h>
@@ -7,5 +9,14 @@
 int main(void) {
 	int a = 10;
 	printf("%d\n", a);
+
+	float b = 123.45F;
+	double c = 123.45;
+	printf("%f\n", b);
+	printf("%f\n", c);
+
+	char d = 'A';
+	printf("%c\n", d);
+
 	return 0;
 }
