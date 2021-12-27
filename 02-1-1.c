9dc2f635d138911bb222079502a8fa5309e1cfc6
kosuha <okeydokekim@gmail.com>
2021-12-27 18:16:07 +0900

    restart

--- 02-1-1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	printf("홍길동\n홍 길 동\n홍  길  동\n");
-
-	return 0;
-}
--- 02-2-2.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	printf("%dx%d=%d\n", 4, 5, 20);
-	printf("%dx%d=%d\n", 7, 9, 63);
-
-	return 0;
-}
